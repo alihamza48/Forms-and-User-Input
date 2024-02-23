cb9c4f6878ab8f675ba663f8a124b53fa9c273f6
Ali Hamza <syednaqvi1881@gmail.com>
2024-02-24 00:45:20 +0500

    first commit

---
+++ src/components/SimpleInput.js
@@ -0,0 +1,76 @@
+import UseInput from "../hooks/use-input";
+
+const SimpleInput = (props) => {
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetName,
+  } = UseInput((value) => value.trim() !== "");
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = UseInput((value) => value.includes("@"));
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid && enteredEmailIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (!enteredNameIsValid) {
+      return;
+    } else if (!enteredEmailIsValid) {
+      return;
+    }
+
+    resetName();
+    resetEmail();
+  };
+
+  const cssClass = nameInputHasError ? "form-control invalid" : "form-control ";
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={cssClass}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameChangeHandler}
+          onBlur={nameBlurHandler}
+          value={enteredName}
+        />
+        <label htmlFor="email">Your Email</label>
+        <input
+          type="text"
+          id="email"
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          value={enteredEmail}
+        />
+        {nameInputHasError && (
+          <p className="error-text">Entered Name is not Valid</p>
+        )}
+        {emailInputHasError && (
+          <p className="error-text">Entered Email is not Valid</p>
+        )}
+      </div>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
